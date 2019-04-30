7a017cb0d220d091024bb2c12b353f9ce3935978
Adel Mahmoud <adelmahmoud1997@gmail.com>
2019-04-30 17:38:17 +0200

    insert_piece function

--- Code/boardGui.py
+++ Code/boardGui.py
@@ -13,6 +13,20 @@ def create_board_matrix(no_rows,no_cols):
     return np.zeros((no_rows,no_cols),dtype= np.int8)
 
 
+def insert_piece(board, col, piece):
+    no_rows, no_cols  = board.shape
+    inserted_flag = 0
+    for i in range(no_rows-1,0,-1):
+        if board[i,col] == 0:
+            board[i,col] = piece
+            inserted_flag = 1
+            break
+        else:
+            continue
+    draw_board(board)
+    return board, inserted_flag
+
+
 def draw_board(board):
     no_rows, no_cols = board.shape
     boardBackground = pygame.display.set_mode((no_cols * SquareLength, (no_rows + 1)  * SquareLength  ))
@@ -34,17 +48,20 @@ def draw_board(board):
                                    int(SquareLength / 2 - 4))
             else:
                 print("Error: unresolved board vlaue")
-
+    pygame.display.update()
 def main():
     board = create_board_matrix(6,7)
     pygame.init()
     draw_board(board)
+    board, inserted = insert_piece(board,0,2)
+    print(board, inserted)
+    insert_piece(board,1,1)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        pygame.display.update()
+
 
 
 if __name__ == "__main__":
